7d8af0d61265686d316c36797f9eae723471fe25
Brandon Thomas Suit <brandon.suit@gmail.com>
2010-07-31 00:22:51 -0400

    Fixed prompt box, formatting...

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -58,7 +58,7 @@ apptags =
 }
 
 -- Define if we want to use titlebar on all applications.
-use_titlebar = true
+use_titlebar = false 
 -- }}}
 
 -- {{{ Tags
@@ -100,8 +100,12 @@ wifi_info = widget({type="textbox"})
 
 vicious.register(date_info, vicious.widgets.date, "%b %d, %R", 60) 
 vicious.register(bat_info, vicious.widgets.bat, "$1$2%", 61, "BAT1")
-vicious.register(vol_info, vicious.widgets.volume, "vol $1%", 2, "Master")
-vicious.register(wifi_info, vicious.widgets.wifi, "${ssid} ${link}% ${rate} Mb/s", 5, "wlan0")
+vicious.register(vol_info, vicious.widgets.volume, 
+					"vol $1%", 2, "Master")
+vicious.register(wifi_info, vicious.widgets.wifi, 
+					"${ssid} ${link}% ${rate} Mb/s", 5, "wlan0")
+
+promptbox = awful.widget.prompt({ align = "left" })
 
 --------------
 -- Bar
@@ -114,7 +118,10 @@ mybar = awful.wibox({
 	border_color = beautiful.border_widget,
 	border_width = beautiful.border_width
 })
+
+-- R to L layout.
 mybar.widgets = { 
+	promptbox,
 	date_info,
 	sep,
 	wifi_info,
@@ -236,29 +243,29 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+    awful.key({ modkey, }, "Left",   awful.tag.viewprev),
+    awful.key({ modkey, }, "Right",  awful.tag.viewnext),
+    awful.key({ modkey, }, "Escape", awful.tag.history.restore),
 
-    awful.key({ modkey,           }, "j",
+    awful.key({ modkey, }, "j",
         function ()
             awful.client.focus.byidx( 1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "k",
+    awful.key({ modkey, }, "k",
         function ()
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show(true)        end),
+    awful.key({ modkey, }, "w", function () mymainmenu:show(true) end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1) end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1) end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus( 1)       end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus(-1)       end),
-    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
-    awful.key({ modkey,           }, "Tab",
+    awful.key({ modkey, "Shift" }, "j", function () awful.client.swap.byidx(1) end),
+    awful.key({ modkey, "Shift" }, "k", function () awful.client.swap.byidx(-1) end),
+    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus(1) end),
+    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus(-1) end),
+    awful.key({ modkey, }, "u", awful.client.urgent.jumpto),
+    awful.key({ modkey, }, "Tab",
         function ()
             awful.client.focus.history.previous()
             if client.focus then
@@ -267,26 +274,26 @@ globalkeys = awful.util.table.join(
         end),
 
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey, }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey, "Shift" }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey, }, "l", function () awful.tag.incmwfact(0.05) end),
+    awful.key({ modkey, }, "h", function () awful.tag.incmwfact(-0.05) end),
+    awful.key({ modkey, "Shift" }, "h", function () awful.tag.incnmaster(1) end),
+    awful.key({ modkey, "Shift" }, "l", function () awful.tag.incnmaster(-1) end),
+    awful.key({ modkey, "Control" }, "h", function () awful.tag.incncol(1) end),
+    awful.key({ modkey, "Control" }, "l", function () awful.tag.incncol(-1) end),
+    awful.key({ modkey, }, "space", function () awful.layout.inc(layouts, 1) end),
+    awful.key({ modkey, "Shift" }, "space", function () awful.layout.inc(layouts, -1) end),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    awful.key({ modkey }, "r", function () promptbox:run() end),
 
     awful.key({ modkey }, "x",
               function ()
                   awful.prompt.run({ prompt = "Run Lua code: " },
-                  mypromptbox[mouse.screen].widget,
+                  promptbox.widget,
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
               end)
